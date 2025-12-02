e0568153ae3faefc5cdca1ee255c353a95afa564
Vanessa Sierra <sierrava212@yahoo.com>
2025-12-01 22:33:11 -0500

    selected DOM elements and initialized country data array

--- rest-countries-api-with-color-theme-switcher-master 2/index.js
+++ rest-countries-api-with-color-theme-switcher-master 2/index.js
@@ -1,29 +1,9 @@
-    const countriesContainer = document.querySelector('.countries-container')
-    
-    fetch(`https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital`)
+// Get HTML elements
+var countriesContainer = document.querySelector(".countries-container");
+var regionFilter = document.getElementById("regionFilter");
+var searchInput = document.getElementById("searchInput");
+var themeButton = document.getElementById("themeToggle");
 
-    .then((res) => res.json())
-    .then((data) => {
-        data.forEach((country) => {
-    //console.log(country.name.nativeName)
-    console.log(country.name.nativeName)
-
-
-    const countryCard = document.createElement('a')
-    countryCard.classList.add('country-card')
-    countryCard.href = `/country.html?name=${country.name.common}`
-    countryCard.innerHTML = `
-            <img src="${country.flags.svg}" alt="${country.name.common}
-            <div class="card-text">
-                <h3 class="card-title">${country.name.common}</h3>
-                <p><b>Population:</b>${country.population.toLocaleString()}</p>
-                <p><b>Region: </b>${country.region}</p>
-                <p><b>Capital: </b>${country.capital?.[0]}</p>
-            </div>
-`
-
-    countriesContainer.append(countryCard)
-  })
-})
-
+// Store all country data
+var allCountriesData = [];
 
