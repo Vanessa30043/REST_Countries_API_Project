fbb8787eecfd3e8a2fade5daeaae7fe6cf0e3c2e
Vanessa Sierra <sierrava212@yahoo.com>
2025-12-01 22:34:44 -0500

    fetch country data from REST API and save it

--- rest-countries-api-with-color-theme-switcher-master 2/index.js
+++ rest-countries-api-with-color-theme-switcher-master 2/index.js
@@ -7,3 +7,19 @@ var themeButton = document.getElementById("themeToggle");
 // Store all country data
 var allCountriesData = [];
 
+//selected DOM elements  and country array//
+
+// Fetch all countries from the API
+fetch("https://restcountries.com/v3.1/all")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    allCountriesData = data; // Save for search/filter
+    showCountries(allCountriesData);
+  })
+  .catch(function(error) {
+    console.log("Error fetching countries:", error);
+  });
+
+  
