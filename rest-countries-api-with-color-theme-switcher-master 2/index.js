3f46f79861c50db103da905bdea9906152c26b4c
Vanessa Sierra <sierrava212@yahoo.com>
2025-12-01 22:48:26 -0500

    added country cards dynamically

--- rest-countries-api-with-color-theme-switcher-master 2/index.js
+++ rest-countries-api-with-color-theme-switcher-master 2/index.js
@@ -1,11 +1,11 @@
 // Get HTML elements
-var countriesContainer = document.querySelector(".countries-container");
-var regionFilter = document.getElementById("regionFilter");
-var searchInput = document.getElementById("searchInput");
-var themeButton = document.getElementById("themeToggle");
+let countriesContainer = document.querySelector(".countries-container");
+let regionFilter = document.getElementById("regionFilter");
+let searchInput = document.getElementById("searchInput");
+let themeButton = document.getElementById("themeToggle");
 
 // Store all country data
-var allCountriesData = [];
+let allCountriesData = [];
 
 //selected DOM elements  and country array//
 
@@ -22,4 +22,40 @@ fetch("https://restcountries.com/v3.1/all")
     console.log("Error fetching countries:", error);
   });
 
-  
+
+ // Function to show countries on the page
+function showCountries(list) {
+  countriesContainer.innerHTML = ""; // Clear previous cards
+
+  for (let i = 0; i < list.length; i++) {
+    let country = list[i];
+
+    let card = document.createElement("a");
+    card.classList.add("country-card");
+    card.href = "country.html?name=" + country.name.common; // link to details
+
+    // Add country content
+    card.innerHTML =
+      '<img src="' +
+      country.flags.svg +
+      '" alt="Flag of ' +
+      country.name.common +
+      '" />' +
+      '<div class="card-text">' +
+      "<h3 class='card-title'>" +
+      country.name.common +
+      "</h3>" +
+      "<p><b>Population:</b> " +
+      country.population.toLocaleString() +
+      "</p>" +
+      "<p><b>Region:</b> " +
+      country.region +
+      "</p>" +
+      "<p><b>Capital:</b> " +
+      (country.capital ? country.capital[0] : "No capital") +
+      "</p>" +
+      "</div>";
+
+    countriesContainer.appendChild(card);
+  }
+}
